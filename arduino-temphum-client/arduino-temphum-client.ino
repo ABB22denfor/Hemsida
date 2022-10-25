fa768c508764f6522304a7f815faab47a040f8d4
Hampus Fridholm <hampus.fridholm@abbgymnasiet.se>
2022-10-25 11:58:34 +0200

    Made the ardiuno code send data correctly. After some re-writing I have come up with some working code.

--- arduino-temphum-client/arduino-temphum-client.ino
+++ arduino-temphum-client/arduino-temphum-client.ino
@@ -5,14 +5,13 @@
 // Declare variables for the temperature-humidity sensor and the Firebase database object
 AM2320 THSensor;
 FirebaseData fbaseData;
-Adafruit_SSD1306 display(128, 64, &Wire, -1, 400000UL, 100000UL);
+Adafruit_SSD1306 display(DISPLAY_WIDTH, DISPLAY_HEIGHT, &Wire, -1, 400000UL, 100000UL);
 
 // The setup function connects to the local WIFI and setups communication with the Firebase database
 void setup() {
   Serial.begin(BAUD_VALUE); // Allocates the Serial Monitor value
   Wire.begin(SENSOR_PIN1, SENSOR_PIN2); // Allocates the pins for the AM2320 sensor
   display.begin(SSD1306_SWITCHCAPVCC, DISPLAY_ADDR);
-  WiFi.mode(WIFI_STA);
 
   // Tries to connect to the local WIFI using the WIFI SSID and Password
   if(!connect_local_wifi(WIFI_SSID, WIFI_PASW))
@@ -26,10 +25,14 @@ void setup() {
   {
     Serial.println("Error: Could not setup and connect to database");
   }
+  else Serial.println("Success! Setup and connected to Firebase!");
 }
 
 // The loop function will execute continuously. Collect values and push them to the Firebase database
-void loop() {
+void loop() 
+{
+  const char timeString[] = "2022-10-20 14:05";
+  
   float sensorTemp = 0.0f, sensorHum = 0.0f; int errorCode;
 
   // Collect temperature and humidity from the AM2320 sensor
@@ -39,60 +42,53 @@ void loop() {
     if(errorCode == 1) Serial.println("Error: Sensor is offline");
     if(errorCode == 2) Serial.println("Error: CRC validation failed");
   }
+  else Serial.println("Collected temperature and humidity from sensor");
 
-  const char timeString[] = "2022-10-20 14:05";
-
-  Serial.print("SensorTemp: "); Serial.print(sensorTemp); 
-  Serial.print("\tSensorHum: "); Serial.print(sensorHum); 
-  Serial.print("\tTimeString: "); Serial.println(timeString);
+  Serial.println("TimeString: " + String(timeString) + "\tSensorTemp: " + String(sensorTemp) + "\tSensorHum: " + String(sensorHum));
   
-  /*if(!display_sensor_values(sensorTemp, sensorHum))
+  if(!display_sensor_values(sensorTemp, sensorHum))
   {
     Serial.println("Error: Could not display sensor values");
-  }*/
+  }
+  else Serial.println("Displayed temperature and humidity on screen");
+
   
   // Tries to push the temperature and humidity to the Firebase database
   if(!push_sensor_values(STREAM_PATH, sensorTemp, sensorHum, timeString))
   {
     // Prints out the error that occured when pushing the values
-    Serial.println("Error" + fbaseData.errorReason());
+    Serial.println("Error: " + fbaseData.errorReason());
   }
+  else Serial.println("Success! Pushed sensor values to Firebase");
+
   delay(LOOP_DELAY);
 }
 
 bool display_sensor_values(float sensorTemp, float sensorHum)
 {
-  display.clearDisplay();
+  display.setTextColor(TEXT_COLOR); 
+  display.setTextSize(TEXT_SIZE);
 
-  display.setTextColor(TEXT_COLOR); display.setTextSize(TEXT_SIZE);
+  display.clearDisplay();
   display.setCursor(0, 0);
 
   display.print("TEMP:"); display.println(sensorTemp);
   display.print("HUM :"); display.println(sensorHum);
 
   display.display();
+
+  return true;
 }
 
-// Takes in the temperature and humidity and creates a JSON object that it pushes to the Firebase database
 bool push_sensor_values(const char streamPath[], float sensorTemp, float sensorHum, const char timeString[])
 {
-  // Creates a JSON package and stores the temperature and the humidity
-  FirebaseJson jsonPackage;
-  if(!create_json_package(&jsonPackage, sensorTemp, sensorHum, timeString))
-  {
-    Serial.println("Error: Could not create json package"); 
-    return false;
-  }
-  // Tries to push the JSON Package to the Firebase Databse
-  return Firebase.pushJSON(fbaseData, streamPath, jsonPackage);
-}
+  char tempPath[128], humPath[128];
 
-// Creates a JSON package and stores the temperature and the humidity
-bool create_json_package(FirebaseJson* jsonPackage, float sensorTemp, float sensorHum, const char timeString[])
-{
-  (*jsonPackage).set("teperature", sensorTemp);
-  (*jsonPackage).set("humidity", sensorHum);
-  (*jsonPackage).set("time", timeString);
+  if(!sprintf(tempPath, "%s/%s/temperature", streamPath, timeString) ||
+      !sprintf(humPath, "%s/%s/humidity", streamPath, timeString)) return false;
+
+  if(!Firebase.setFloat(fbaseData, tempPath, sensorTemp) ||
+      !Firebase.setFloat(fbaseData, humPath, sensorHum)) return false;
 
   return true;
 }
@@ -101,10 +97,13 @@ bool create_json_package(FirebaseJson* jsonPackage, float sensorTemp, float sens
 bool get_sensor_values(float* sensorTemp, float* sensorHum, int* errorCode)
 {
   // Checks if the sensor is active and will measure temperature and humidity
-  if(!THSensor.measure()) { *errorCode = THSensor.getErrorCode(); return false; }
-
+  if(!THSensor.measure()) 
+  { 
+    *errorCode = THSensor.getErrorCode(); 
+    return false; 
+  }
   // Collect temperature and humidity and store the value at the pointers
-  *sensorTemp = THSensor.getTemperature();
+  *sensorTemp = THSensor.getTemperature(); 
   *sensorHum = THSensor.getHumidity();
 
   return true;
@@ -113,6 +112,8 @@ bool get_sensor_values(float* sensorTemp, float* sensorHum, int* errorCode)
 // Tries to connect to local WIFI using WIFI SSID and Password, stores the local address at pointer
 bool connect_local_wifi(const char wifiSSID[], const char wifiPasw[])
 {
+  WiFi.mode(WIFI_STA);
+
   if (WiFi.status() == WL_NO_SHIELD) 
   {
       Serial.println("Error: WiFi shield not present");
@@ -124,7 +125,10 @@ bool connect_local_wifi(const char wifiSSID[], const char wifiPasw[])
   for(int index = 0; WiFi.status() != WL_CONNECTED; index += 1)
   {
     Serial.print("Connecting to WIFI: "); Serial.println(index + 1);
-    if(index >= CONNECT_TRIES) break; delay(WIFI_DELAY);
+
+    if(index >= CONNECT_TRIES) break; 
+    
+    delay(WIFI_DELAY);
   }
   return (WiFi.status() == WL_CONNECTED);
 }
--- arduino-temphum-client/temphum-client-defines.h
+++ arduino-temphum-client/temphum-client-defines.h
@@ -8,12 +8,12 @@ const char DBASE_AUTH[] = "f17uoDIiCqTEs929Ak1TnkMK7fNBYV3LEFoUbDXW";
 const char WIFI_SSID[] = "ABBgym_2.4";
 const char WIFI_PASW[] = "mittwifiarsabra";
 
-const unsigned int CONNECT_TRIES = 100;
+const unsigned int CONNECT_TRIES = 64;
 
-const char STREAM_PATH[] = "/stream";
+const char STREAM_PATH[] = "/";
 
 const unsigned int LOOP_DELAY = 1000;
-const unsigned int WIFI_DELAY = 300;
+const unsigned int WIFI_DELAY = 1000;
 
 const unsigned int BAUD_VALUE = 9600;
 
@@ -21,6 +21,9 @@ const unsigned int SENSOR_PIN1 = 14;
 const unsigned int SENSOR_PIN2 = 12;
 
 const int DISPLAY_ADDR = 0x3C;
+
+const unsigned int DISPLAY_WIDTH = 128;
+const unsigned int DISPLAY_HEIGHT = 64;
 
 const unsigned int TEXT_SIZE = 2;
 const uint16_t TEXT_COLOR = WHITE;
