75e2a54c88d7f9449943d8a4600418b429c4f0ff
Hampus Fridholm <hampus.fridholm@abbgymnasiet.se>
2022-11-08 09:08:09 +0100

    Delete unneccessary files and refacotred Lysanders hemsida code.

--- hemsida.js
+++ hemsida.js
@@ -1,20 +1,6 @@
-/*document.getElementById("countValue").innerText = "1";
-document.getElementById("tempValue").innerText = "2";
-document.getElementById("totalValue").innerText = "3";
-document.getElementById("perValue").innerText = "4";
-document.getElementById("lastValue").innerText = "5";
-document.getElementById("humValue").innerText = "6";*/
 
-var checkbox1 = document.getElementById('checkbox1');
-var adv = document.getElementById('adv');
-var none = document.getElementById('none');
+var advCheckbox = document.getElementById('checkbox1');
+var advancedBox = document.getElementById('adv');
 
-none.style.display = 'none';
-
-checkbox1.onchange = function () {
-  if (checkbox1.checked) {
-    adv.style.display = "flex";
-  } else {
-    adv.style.display = "none";
-  }
-};
+advCheckbox.onchange = () =>
+{ advancedBox.style.display = (advCheckbox.checked) ? "flex" : "none"; };
