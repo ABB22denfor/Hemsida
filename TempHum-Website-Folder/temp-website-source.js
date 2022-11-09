fd164385e913e79cf4257f86f1138533304aa12f
Hampus Fridholm <hampus.fridholm@abbgymnasiet.se>
2022-11-09 18:39:22 +0100

    Lysander added a duplicate file when he commited. I removed it and put his progress (commenting) in the right file.

--- TempHum-Website-Folder/temp-website-source.js
+++ TempHum-Website-Folder/temp-website-source.js
@@ -2,5 +2,6 @@
 var advCheckbox = document.getElementById('checkbox1');
 var advancedBox = document.getElementById('adv');
 
+//Används för att få Advanced knappen att visa en viss div
 advCheckbox.onchange = () =>
 { advancedBox.style.display = (advCheckbox.checked) ? "flex" : "none"; };
