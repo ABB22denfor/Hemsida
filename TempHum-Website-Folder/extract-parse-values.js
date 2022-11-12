039dbd8972edf18fcae52a70463ef3c6013f2a56
ABBNojGau <Nojus.gaupsas@abbgymnasiet.se>
2022-11-13 00:02:30 +0100

    lade till kommentarer oc to bort onödigt

--- TempHum-Website-Folder/extract-parse-values.js
+++ TempHum-Website-Folder/extract-parse-values.js
@@ -14,54 +14,61 @@ firebase.initializeApp(firebaseConfig); // importar firebase så att vi kan läs
 
 var openedCount = 0;
 var dataPoints = [];
-var timeArray = [];
+var timeArray = []; //Definerar de två arrays som vi använder
 
 dataBase = firebase.database();
 
-var streamData = dataBase.ref("/"); //Säger var koden ska läsa.
+var streamData = dataBase.ref("/"); //Säger var koden ska läsa på firebase.
 
 streamData.on("child_added", (snapshot) => {
-  // När en ny mätning kommer in, så ska denna kod köras.
-  const dataPoint = snapshot.val();
+  const dataPoint = snapshot.val(); // ger dataPoint valuerna av den nya mätningen på firebase.
 
-  dataPoints.push(dataPoint);
+  dataPoints.push(dataPoint); // lägger till det som ligger i den nya mätningen till arryn med mätningarna.
 
-  let [tempValue, humValue, countValue, perValue, lastValue] = create_page_values(dataPoints);
+  let [tempValue, humValue, countValue, perValue, lastValue] =
+    create_page_values(dataPoints); // använder funktionen för att ge varieblerna värden.
 
   console.log(
     `tempvalue: ${tempValue}   
       humValue: ${humValue}
      countValue: ${countValue}`
-  );
+  ); // visar temperaturen, humidityn och hur många gånger kylskåpet har öppnats i consolen för debugging.
 
-  update_page_values(tempValue, humValue, countValue, totalValue, perValue, lastValue);
-});
+  update_page_values(
+    tempValue,
+    humValue,
+    countValue,
+    totalValue,
+    perValue,
+    lastValue // uppdaterar valuerna med hjälp av en funktion
+  );
+}); // Loopen som körs varje gång en ny mätning kommer in.
 
 function calculate_total_time(time) {
-  totalValue =(time - 1667928163)/3600
+  totalValue = (time - 1667928163) / 3600;
   totalValue = round_values(totalValue, 1);
-  return (totalValue)
-}
+  return totalValue;
+} // en funktion för att ränka ut länge det har gått sen arduinon startade mäta
 
 function calculate_per_hour(countValue, totalValue) {
   openPerHour = countValue / totalValue;
   openPerHour = round_values(openPerHour, 1);
-  return (openPerHour);
-}
+  return openPerHour;
+} // ränkar hur många gånger kylskåpet har öppnats per timme genom att dela hur många gånger den har öppnats med totala timmar.
 
 function calculate_last_opened(currentTime, currentTemp, prevTemp) {
   let lastValue;
   if (currentTemp <= 8.5 && prevTemp >= 8.6) {
-    lastValue = currentTime
+    lastValue = currentTime;
     console.log(`lastValue: ${lastValue}`);
     return lastValue;
-    
   }
   return 0;
-}
+} // räknar hur länge det var sedan kylskåpet öppnades. Den gör det genom att ha samma condiitoner som countValue, och sedan göra current time till lastValue.
+
 /* streamData.once("value", (snapshot) => {
   snapshot.ref.remove();
-}); */
+}); */ // kod som Jeton skrev för att ta bort allting i firebase ifall det blev fullt.
 
 function update_count_value(dataPoints, countValue) {
   let currentTemp = dataPoints[dataPoints.length - 1].temperature;
@@ -72,12 +79,13 @@ function update_count_value(dataPoints, countValue) {
 
   if (currentTemp >= 8.6 && prevTemp <= 8.5) return countValue + 1;
   else return countValue;
-}
-function round_values(value, precision){
-  var multiplier = Math.pow(10, precision || 0)
+} // uppdaterar countValue som visar hur ofta kylskåpet har öppnats genom att kolla om den senaste och förra temperaturen möter konditionerna tillsamans.
+
+function round_values(value, precision) {
+  var multiplier = Math.pow(10, precision || 0);
   value = Math.round(value * multiplier) / multiplier;
-  return value
-}
+  return value;
+} // funktion som rundar av values
 
 function create_page_values(dataPoints) {
   let dataPoint = dataPoints[dataPoints.length - 1];
@@ -88,20 +96,16 @@ function create_page_values(dataPoints) {
   let currentHum = dataPoint.humidity;
   let currentTime = dataPoint.epochTime;
 
-
   let prevTemp;
   if (dataPoints.length < 2) prevTemp = currentTemp;
   else prevTemp = dataPoints[dataPoints.length - 2].temperature;
 
-  
   let totalValue = calculate_total_time(currentTime);
   let perValue = calculate_per_hour(openedCount, totalValue);
   let lastValue = calculate_last_opened(currentTime, currentTemp, prevTemp);
   console.log(`Last Opened: ${lastValue}`);
 
-  let stringTime = JSON.stringify(dataPoint.epochTime);
-
-  let TimeBetween = (currentTime - dataPoints[0].epochTime);
+  let TimeBetween = currentTime - dataPoints[0].epochTime;
 
   let Timmar = (TimeBetween - (TimeBetween % 3600)) / 3600;
   console.log(`Timmar: ${Timmar}`);
@@ -112,12 +116,18 @@ function create_page_values(dataPoints) {
   return [currentTemp, currentHum, openedCount, perValue, lastValue];
 }
 
-function update_graph(time){
+function update_graph(time) {
   timeArray.push(time);
 }
 
-function update_page_values(tempValue, humValue, countValue, totalValue, perValue, lastValue) 
-{
+function update_page_values(
+  tempValue,
+  humValue,
+  countValue,
+  totalValue,
+  perValue,
+  lastValue
+) {
   document.getElementById("tempValue").innerHTML = tempValue;
 
   document.getElementById("humValue").innerHTML = humValue;
